55f00bbdc4338aaf6e306e683b51f9952ea0a1be
erie0210 <eriee0210@gmail.com>
2021-11-22 23:46:24 +0900

    feat(secret): 보안을 위해 secret 파일 생성

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,14 +5,16 @@ from flask.templating import render_template
 from werkzeug.utils import redirect
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app(test_config=None):
+from secret import DB
 
+def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config['SECRET_KEY'] = 'dev'
-    app.config['JSON_AS_ASCII'] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:adminadmin@database.cpzfde8y7eey.ap-northeast-2.rds.amazonaws.com:3306/database?charset=utf8"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config.from_mapping(
+      SECRET_KEY='dev',
+      SQLALCHEMY_DATABASE_URI=DB['DATABASE_URI'],
+      SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
 
     db = SQLAlchemy()
     db.init_app(app)
---
+++ secret.py
@@ -0,0 +1,7 @@
+DB=dict()
+DB["HOST"]="database.cpzfde8y7eey.ap-northeast-2.rds.amazonaws.com"
+DB["PORT"]="3306"
+DB["USER"]="admin"
+DB["PASSWORD"]="adminadmin"
+DB["DATABASE"]="database"
+DB["DATABASE_URI"]="mysql+pymysql://admin:adminadmin@database.cpzfde8y7eey.ap-northeast-2.rds.amazonaws.com:3306/database?charset=utf8"
