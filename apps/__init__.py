2e91c4d1da8fa87ececd706e79f44e65768f15e5
erie0210 <eriee0210@gmail.com>
2021-11-23 11:32:37 +0900

    feat(swagger): swagger 기능 추가

--- apps/__init__.py
+++ apps/__init__.py
@@ -10,9 +10,12 @@ from sqlalchemy    import create_engine
 from config import ProductionConfig, DevelopmentConfig, TestingConfig
 from flask_sqlalchemy import SQLAlchemy
 
+from flasgger import Swagger
 
 def create_app(test_config=None):
+
     app = Flask(__name__, instance_relative_config=True)
+    swagger = Swagger(app)
 
     # config
     if os.environ.get('WORK_ENV') == 'PROD':
@@ -25,9 +28,10 @@ def create_app(test_config=None):
 
     db = SQLAlchemy()
     # db.init_app(app)
+    # db
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     # AUTH: 인증 관련 블루프린트 
     from . import auth
@@ -37,5 +41,9 @@ def create_app(test_config=None):
     from . import balance
     app.register_blueprint(balance.bp)
     app.add_url_rule('/', endpoint='index')
+
+    @app.route('/docs')
+    def index():
+        return {"message" : "Welcome!"}
 
     return app
