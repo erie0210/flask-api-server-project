9a4aa5c7361ea47dfad1063fe8e91c39d0cacae1
erie0210 <eriee0210@gmail.com>
2021-11-23 11:47:24 +0900

    fix: testconfig의 database url 변경

--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:12345678@127.0.0.1:3306/test"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:adminadmin@database.cpzfde8y7eey.ap-northeast-2.rds.amazonaws.com:3306/test?charset=utf8"
     SQLALCHEMY_ECHO = False
     SECRET_KEY = 'SECRET-KEY'
